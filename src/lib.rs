c0080a29bb2c86b893b2edbd389073fff594ab2b
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-06-29 22:24:28 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+/// Adds two integers
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_positive() {
+        assert_eq!(add(2, 3), 5);
+    }
+
+    #[test]
+    fn test_add_negative() {
+        assert_eq!(add(-2, -3), -5);
+    }
+
+    #[test]
+    fn test_add_fail() {
+        assert_eq!(add(1, 1), 42);
+    }
+}
